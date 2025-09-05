d02304118d58d2fa344c1ccb60ea1b1ec7681c13
Juhyung Park <qkrwngud825@gmail.com>
2025-09-04 21:35:16 -0700

    add gdb script
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

---
+++ cxl_scripts/start-ubuntu-gdb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd ../build
+gdb --args ./qemu-system-x86_64 \
+    --trace "cxl_root*" \
+    --trace "cxl_read*" \
+    --trace "cxl_write*" \
+    --trace "cxl_usp*" \
+    --trace "cxl_debug*" \
+    --trace "cxl_socket_cxl_io*" \
+    --trace "qdev_device*" \
+    --trace "pc_debug*" \
+    --trace "vl_debug*" \
+    --trace "pci_debug*" \
+        -D debug.log \
+	-m 8G -smp 1 \
+	-machine type=q35,accel=kvm,cxl=on -nographic \
+	-hda /home/arter97/lab/opencis/images/ubuntu-noble1.qcow2 \
+	-D debug.log \
+	-device pxb-cxl,bus_nr=12,bus=pcie.0,id=cxl.1 \
+	-device cxl-rp,port=0,bus=cxl.1,id=root_port0,chassis=0,slot=2,switch-port=0,socket-host=0.0.0.0 \
+	-M "cxl-fmw.0.targets.0=cxl.1,cxl-fmw.0.size=512G" \
+	-nic user,id=vmnic,hostfwd=tcp::2223-:22
